733e75ed9147e429d8c29e72f284197319eeb5c7
Kapil444 <45959669+Kapil444@users.noreply.github.com>
2021-01-01 09:20:23 +0530

    Create index.js

---
+++ Web Development/week 9/Quote of the day app/index.js
@@ -0,0 +1,29 @@
+if(new Date().getDay()==1){
+    console.log("You’re off to great places, today is your day. Your mountain is waiting, so get on your way \n");    
+    console.log("Dr. Seuss \n");    
+    console.log("Author/Poet \n");
+}else if(new Date().getDate()==2){
+    console.log("You always pass failure on the way to success. \n");    
+    console.log("Mickey Rooney \n");    
+    console.log("Actor \n");
+}else if(new Date().getDate()==3){
+    console.log("No one is perfect - that’s why pencils have erasers. \n");    
+    console.log("Wolfgang Riebe \n");    
+    console.log("Keynote Speaker/Magician \n");
+}else if(new Date().getDate()==4){
+    console.log("Winning doesn’t always mean being first. Winning means you’re doing better than you’ve done before.. \n");    
+    console.log("Bonnie Blair \n");    
+    console.log("Speed Skater \n");
+}else if(new Date().getDate()==5){
+    console.log("You’re braver than you believe, and stronger than you seem, and smarter than you think.. \n");    
+    console.log("A.A. Mine \n");    
+    console.log("Author/Poet \n");
+}else if(new Date().getDate()==6){
+    console.log("It always seems impossible until it is done. \n");    
+    console.log("Nelson Mandela \n");    
+    console.log("Political Leader \n");
+}else if(new Date().getDate()==7){
+    console.log("Keep your face to the sunshine and you cannot see a shadow. \n");    
+    console.log("Helen Keller \n");    
+    console.log("Author \n");
+}
